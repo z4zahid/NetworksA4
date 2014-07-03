293def210c1102945ee75563bfa0f7b540a922a6
SARAH CHAN <s44chan@uwaterloo.ca>
2014-07-03 04:39:23 -0400

    Added implementation to accept and connect. Haven't tested it out yet though so it might die

--- rcs.cc
+++ rcs.cc
@@ -1,6 +1,20 @@
 #include <iostream>
 
+#define BUFFER_SIZE 259
+#define SYN_BIT 0
+#define ACK_BIT 1
+#define SEQ_NUM 2
+#define ACK_NUM 3
+#define DATA_INDEX 4
+
+#define ACK_SET 'a'
+#define SYN_SET 's'
+
+
 using namespace std;
+
+char serverSeq = 0;
+char clientSeq = 0;
 
 /*
 server invokes: rcsSocket(), rcsBind(), rcsListen(), rcsAccept(), rcsRecv(), rcsSend() and rcsClose()
@@ -41,13 +55,76 @@ int rcsListen(int socketID) {
 // The address of the peer (client) is filled into the second argument. 
 // Returns descriptor to new RCS socket that can be used to rcsSend() and rcsRecv() with the peer (client).
 int rcsAccept(int socketID, struct sockaddr_in * addr) {
+    char receiveBuf[BUFFER_SIZE];
 
+    // Blocking call until we get SYN request
+    while (true) {
+        ucpRecvFrom(socketID, receiveBuf, BUFFER_SIZE, &addr);
+        if (receiveBuf[SYN_BIT] == SYN_SET) {
+            break;
+        }
+    }
+    
+    // Set the sequence number and ACK of the buffer
+    char seq_num = serverSeq++;
+    char ack_num = receiveBuf[SEQ_NUM] + 1;
+    char sendBuf[BUFFER_SIZE];
+    sendBuf[SYN_BIT] = SYN_SET;
+    sendBuf[ACK_INDEX] = ACK_BIT;
+    sendBuf[ACK_NUM] = ack_num;
+    sendBuf[SEQ_BIT] = seq_num;
+
+    ucpSetSockRecvTimeout(socketID, 1000);
+    memset(receiveBuf, 0, BUFFER_SIZE);
+    
+    // Send a SYNACK to the client and
+    // Wait for an ACK from client
+    struct sockaddr_in * ackAddr;
+    while (true) {
+        ucpSendTo(socketID, sendBuf, BUFFER_SIZE, addr);
+        ucpRecvFrom(socketID, receiveBuf, BUFFER_SIZE, &ackAddr);
+        if (receiveBuf[ACK_INDEX] == ACK_SET && receiveBuf[ACK_NUM] == seq_num + 1) {
+            break;
+        }
+    }
+    return 0;
 }
 
 // connects a client to a server. The socket (first argument) must have been bound beforehand using rcsBind().
 // The second argument identifies the server to which connection should be attempted
 int rcsConnect(int socketID, const struct sockaddr_in * addr) {
+    
+    // Set a timeout
+    ucpSetSockRecvTimeout(socketID, 3000);
+    
+    char buf[BUFFER_SIZE];
+    char seq_num = clientSeq++;
+    buf[SYN_BIT] = SYN_SET;
+    buf[SEQ_NUM] = seq_num;
+    
+    char receiveBuf[BUFFER_SIZE];
+    
+    // Send SYN to server and wait for ACK from the server
+    while (true) {
+        ucpSendTo(socketID, buf, BUFFER_SIZE, addr);
+        ucpRecvFrom(socketID, receiveBuf, BUFFER_SIZE, &addr);
+        if (receiveBuf[ACK_INDEX] == ACK_SET && receiveBuf[ACK_NUM] == seq_num + 1) {
+            break;
+        }
+    }
+    
+    memset(buf, 0, BUFFER_SIZE);
+    char ack_num = receiveBuf[SEQ_NUM] + 1;
+    buf[ACK_NUM] = ack_num;
+    buf[ACK_INDEX] = ACK_SET;
+        
+    memset(receiveBuf, 0, BUFFER_SIZE);
+    
+    // Send ACK to the server
+    ucpSendTo(socketID, buf, BUFFER_SIZE, addr);
 
+    // TODO: maybe keep sending until timeout and zero messages from server?
+    
 	//success
 	return 0;
 }
@@ -59,6 +136,7 @@ int rcsConnect(int socketID, const struct sockaddr_in * addr) {
 // returned by this call should be what was sent, and in the correct order.
 int rcsRecv(int socketID, void * rcvBuffer; int maxBytes) {
 
+    // TODO send missing ACK?
 
 } 
 
@@ -73,8 +151,6 @@ int rcsSend(int socketID, const void * sendBuffer; int numBytes) {
 
 //closes an RCS socket descriptor
 int rcsClose(int socketID) {
-
-	//success
-	return 0;
+	return ucpClose(socketID);
 } 
 
