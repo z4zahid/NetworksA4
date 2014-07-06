e5fa90c56fca1f0f20882f5980ead347eef2fb84
SARAH CHAN <s44chan@uwaterloo.ca>
2014-07-05 22:36:17 -0400

    Created makefile and made adjustments with RCS accept and connect

--- Makefile
+++ Makefile
@@ -1,3 +1,13 @@
 # makefile must have the following two targets:
 # clean = remove all .o, executable and .a files
 # librcs.a. archive file that has all the RCS functions
+
+XCC = g++
+CFLAGS  = -c
+SOURCES = ucp_given.c rcs.cc
+
+librcs.a: ucp_given.o rcs.o
+	ar -rcs librcs.a ucp_given.o rcs.o
+
+clean:
+	-rm -f *.o *.a
--- rcs.cc
+++ rcs.cc
@@ -1,20 +1,41 @@
 #include <iostream>
+#include <vector>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-#define BUFFER_SIZE 259
+#include "ucp_given.c"
+
+
 #define SYN_BIT 0
 #define ACK_BIT 1
 #define SEQ_NUM 2
 #define ACK_NUM 3
 #define DATA_INDEX 4
+#define HEADER_SIZE 5
+
+#define BUFFER_SIZE 517 // 512 + HEADER_SIZE
 
 #define ACK_SET 'a'
 #define SYN_SET 's'
 
+// MUTEX
 
 using namespace std;
 
 char serverSeq = 0;
 char clientSeq = 0;
+
+// vector of sockets
+vector<struct sockaddr_in> sockets;
+
+struct Connection {
+	sockaddr_in sendTo;
+	int ack;
+	int seq;
+	int fd;
+};
+
 
 /*
 server invokes: rcsSocket(), rcsBind(), rcsListen(), rcsAccept(), rcsRecv(), rcsSend() and rcsClose()
@@ -30,7 +51,7 @@ int rcsSocket() {
 
 //binds an RCS socket (first argument) to the address structure (second argument)
 int rcsBind(int socketID, const struct sockaddr_in * addr) {
-    return ucpBind(socketID, addr)
+    return ucpBind(socketID, addr);
 }
 
 //fills address information into the second argument with which an RCS socket (first argument) has been 
@@ -54,13 +75,15 @@ int rcsListen(int socketID) {
 // The call is unblocked when a connection request is received. 
 // The address of the peer (client) is filled into the second argument. 
 // Returns descriptor to new RCS socket that can be used to rcsSend() and rcsRecv() with the peer (client).
-int rcsAccept(int socketID, struct sockaddr_in * addr) {
+int rcsAccept(int socketID, struct sockaddr_in *addr) {
     char receiveBuf[BUFFER_SIZE];
 
     // Blocking call until we get SYN request
     while (true) {
-        ucpRecvFrom(socketID, receiveBuf, BUFFER_SIZE, &addr);
+        ucpRecvFrom(socketID, receiveBuf, BUFFER_SIZE, addr);
         if (receiveBuf[SYN_BIT] == SYN_SET) {
+            // Add SYN to th
+            sockets.push_back(*addr);
             break;
         }
     }
@@ -70,9 +93,9 @@ int rcsAccept(int socketID, struct sockaddr_in * addr) {
     char ack_num = receiveBuf[SEQ_NUM] + 1;
     char sendBuf[BUFFER_SIZE];
     sendBuf[SYN_BIT] = SYN_SET;
-    sendBuf[ACK_INDEX] = ACK_BIT;
+    sendBuf[ACK_BIT] = ACK_SET;
     sendBuf[ACK_NUM] = ack_num;
-    sendBuf[SEQ_BIT] = seq_num;
+    sendBuf[SEQ_NUM] = seq_num;
 
     ucpSetSockRecvTimeout(socketID, 1000);
     memset(receiveBuf, 0, BUFFER_SIZE);
@@ -82,8 +105,8 @@ int rcsAccept(int socketID, struct sockaddr_in * addr) {
     struct sockaddr_in * ackAddr;
     while (true) {
         ucpSendTo(socketID, sendBuf, BUFFER_SIZE, addr);
-        ucpRecvFrom(socketID, receiveBuf, BUFFER_SIZE, &ackAddr);
-        if (receiveBuf[ACK_INDEX] == ACK_SET && receiveBuf[ACK_NUM] == seq_num + 1) {
+        ucpRecvFrom(socketID, receiveBuf, BUFFER_SIZE, ackAddr);
+        if (receiveBuf[ACK_BIT] == ACK_SET && receiveBuf[ACK_NUM] == seq_num + 1) {
             break;
         }
     }
@@ -103,12 +126,13 @@ int rcsConnect(int socketID, const struct sockaddr_in * addr) {
     buf[SEQ_NUM] = seq_num;
     
     char receiveBuf[BUFFER_SIZE];
+    struct sockaddr_in serverAddr = *addr;
     
     // Send SYN to server and wait for ACK from the server
     while (true) {
         ucpSendTo(socketID, buf, BUFFER_SIZE, addr);
-        ucpRecvFrom(socketID, receiveBuf, BUFFER_SIZE, &addr);
-        if (receiveBuf[ACK_INDEX] == ACK_SET && receiveBuf[ACK_NUM] == seq_num + 1) {
+        ucpRecvFrom(socketID, receiveBuf, BUFFER_SIZE, &serverAddr);
+        if (receiveBuf[ACK_BIT] == ACK_SET && receiveBuf[ACK_NUM] == seq_num + 1) {
             break;
         }
     }
@@ -116,14 +140,13 @@ int rcsConnect(int socketID, const struct sockaddr_in * addr) {
     memset(buf, 0, BUFFER_SIZE);
     char ack_num = receiveBuf[SEQ_NUM] + 1;
     buf[ACK_NUM] = ack_num;
-    buf[ACK_INDEX] = ACK_SET;
+    buf[ACK_BIT] = ACK_SET;
         
     memset(receiveBuf, 0, BUFFER_SIZE);
     
     // Send ACK to the server
     ucpSendTo(socketID, buf, BUFFER_SIZE, addr);
 
-    // TODO: maybe keep sending until timeout and zero messages from server?
     
 	//success
 	return 0;
@@ -134,9 +157,13 @@ int rcsConnect(int socketID, const struct sockaddr_in * addr) {
 // Returns the actual amount of data received. “Amount” is the number of bytes. 
 // Data is sent and received reliably, so any byte that is
 // returned by this call should be what was sent, and in the correct order.
-int rcsRecv(int socketID, void * rcvBuffer; int maxBytes) {
+int rcsRecv(int socketID, void * rcvBuffer, int maxBytes) {
 
-    // TODO send missing ACK?
+    // Note to Zainab: there's a case where the final handshake ACK may be lost
+    // In that case, server will send us a SYNACK
+    // If that's the case, you will need to send the missing ACK as well as the message
+    // Feel free to bug me if there's any issues
+    return 0;
 
 } 
 
@@ -144,8 +171,8 @@ int rcsRecv(int socketID, void * rcvBuffer; int maxBytes) {
 // blocks sending data. 
 // Returns the actual number of bytes sent. If rcsSend() returns with a
 // non-negative return value, then we know that so many bytes were reliably received by the other end
-int rcsSend(int socketID, const void * sendBuffer; int numBytes) {
-
+int rcsSend(int socketID, const void * sendBuffer, int numBytes) {
+    return 0;
 
 } 
 
