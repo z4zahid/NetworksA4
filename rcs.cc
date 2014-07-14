b9e96ef178164e26677fb2b9c6b12e7859d0a2e4
z3zahid <zainabzahid4@gmail.com>
2014-07-14 13:46:14 -0400

    initial implementation of send/receive, does not compile at this stage, but most of the logic is here, with some TODOs scattered everywhere

--- rcs.cc
+++ rcs.cc
@@ -20,13 +20,17 @@
 #define ACK_SET 'a'
 #define SYN_SET 's'
 
+//must be less than or equal to half the size of the sequence number space
+#define WINDOW_SIZE 5
+#define MAX_PACKET_SIZE 800
+
 using namespace std;
 
-struct Connection {
-	sockaddr_in destination;
+typedef struct connection {
+    sockaddr_in destination;
     int socketID;
-	bool ack;
-};
+    bool ack;
+} Connection;
 
 char serverSeq = 0;
 char clientSeq = 0;
@@ -34,10 +38,17 @@ char clientSeq = 0;
 pthread_mutex_t lock;
 int counter;
 
-// vector of sockets
-vector<struct Connection> connections;
+//send/rcv 
+typedef struct datapacket {
+    void* data;
+    int sequenceNum;
+    int checksum;
+} DataPacket;
 
-void addConnection(struct Connection connection) {
+// vector of sockets
+vector<Connection> connections;
+
+void addConnection(Connection connection) {
     pthread_mutex_lock(&lock);
     connections.push_back(connection);
     pthread_mutex_unlock(&lock);
@@ -54,6 +65,15 @@ void removeConnection(int socketID) {
     pthread_mutex_unlock(&lock);
 }
 
+sockaddr_in getConnectionAddr(int socketID) {
+    pthread_mutex_lock(&lock);
+    for (int i = 0 ; i < connections.size(); i++) {
+        if ((connections.at(i)).socketID == socketID) {
+            return connections.at(i).destination;
+        }
+    }
+    pthread_mutex_unlock(&lock);
+}
 
 /*
 server invokes: rcsSocket(), rcsBind(), rcsListen(), rcsAccept(), rcsRecv(), rcsSend() and rcsClose()
@@ -64,7 +84,7 @@ Each should return -1 on error and set errno appropriately.
 
 //used to allocate an RCS socket. Returns a socket descriptor (positive integer) on success
 int rcsSocket() {
-	return ucpSocket();
+    return ucpSocket();
 } 
 
 //binds an RCS socket (first argument) to the address structure (second argument)
@@ -81,8 +101,8 @@ int rcsGetSockName(int socketID, struct sockaddr_in * addr){
 //marks an RCS socket (the argument) as listening for connection requests
 int rcsListen(int socketID) {
 
-	//success
-	return 0;
+    //success
+    return 0;
 }
 
 
@@ -167,10 +187,9 @@ int rcsConnect(int socketID, const struct sockaddr_in * addr) {
     // Send ACK to the server
     ucpSendTo(socketID, buf, BUFFER_SIZE, addr);
     
-	//success
-	return 0;
+    //success
+    return 0;
 }
-
 
 // blocks awaiting data on a socket (first argument).
 // Returns the actual amount of data received. “Amount” is the number of bytes. 
@@ -178,21 +197,160 @@ int rcsConnect(int socketID, const struct sockaddr_in * addr) {
 // returned by this call should be what was sent, and in the correct order.
 int rcsRecv(int socketID, void * rcvBuffer, int maxBytes) {
 
-    // Note to Zainab: there's a case where the final handshake ACK may be lost
-    // In that case, server will send us a SYNACK
-    // If that's the case, you will need to send the missing ACK as well as the message
-    // Feel free to bug me if there's any issues
-    return 0;
+    DataPacket packet;
 
+    int ackPacket[1];
+
+    //circular buffer?
+    DataPacket ackBuffer[BUFFER_SIZE];
+    int readIndex = 0;
+    int insertIndex = 0;
+
+    int bytesReceived = 0;
+    int curSequenceNum = 0;
+    int rcvBase = 0;
+    int rcvBaseHi = (rcvBase + WINDOW_SIZE - 1);
+
+    //TODO: what's our end condition? how do we know we've received the entire message?
+    while (true) {
+        
+        struct sockaddr_in addr;
+        ucpRecvFrom(socketID, packet, maxBytes, &addr);
+        
+        if (packet.sequenceNum >= rcvBase && packet.sequenceNum <= rcvBaseHi) {
+
+            if (packet.checksum + getSum(packet.data) == 0) {
+
+                ackPacket[0] = packet.sequenceNum;
+                ucpSendTo(socketID, ackPacket, 1, addr);
+
+                ackBuffer[insertIndex] = packet;
+                insertIndex = (insertIndex + 1) % BUFFER_SIZE;
+
+                if (curSequenceNum == rcvBase) {
+                    // deliver this packet and any previously buffered and consecutively numbered packets
+                    while(readIndex != curSequenceNum) {
+                        rcvBuffer[curSequenceNum] = ackBuffer[readIndex].packet;
+                        readIndex = (readIndex + 1) % BUFFER_SIZE; 
+                    }
+                    // moved receive window forward by the number of packets delivered
+                    rcvBase++;
+                    rcvBaseHi++;
+                } 
+                curSequenceNum++;
+            }
+        } else if (packet.sequenceNum >= (rcvBase - WINDOW_SIZE) && packet.sequenceNum <= (rcvBase - 1)) {
+            //return ACK to sender even though we've already ACKED before
+            ackPacket[0] = packet.sequenceNum;
+            ucpSendTo(socketID, ackPacket, 1, addr); 
+        } else {
+            //ignore
+        }
+    }
+
+    return bytesReceived;
 } 
+    
 
+int getSum(const void* packet){
+    //we want to sum all the words
 
+}
+
+// checksum = 1s complement of the sum of all the bits in the segment with any overflow 
+// encountered during the sum being wrapped around.
+int getChecksum(const void* packet) {
+    return ~(getSum(packet));
+}
+
+// use memset to get the next x bytes from sendBuffer
+// create data packet with the next x number of bytes
+void* getNextPacket(const void* sendBuffer, int seqNum) {
+
+}
+
+/* evilKind == 0 -- send only part of the bytes 
+ *             1 -- corrupt some of the bytes
+ *             2 -- pretend that we sent all the bytes
+ */
 // blocks sending data. 
 // Returns the actual number of bytes sent. If rcsSend() returns with a
 // non-negative return value, then we know that so many bytes were reliably received by the other end
 int rcsSend(int socketID, const void * sendBuffer, int numBytes) {
-    return 0;
+    
+    //TODO: whats the maximum number of times we retransmit a packet before we ignore it?
+    
+    int numPackets = numBytes/MAX_PACKET_SIZE;
+    if (numBytes % MAX_PACKET_SIZE > 0) {
+        numPackets++;
+    }
 
+    //let's send out all the packets and then we can wait for them
+    int curSequenceNum = 0, i;
+    for (i = 0; i<numPackets;i++) {
+
+        DataPacket dataPacket;
+        dataPacket.sequenceNum = curSequenceNum;
+        dataPacket.packet = getNextPacket(sendBuffer, curSequenceNum);
+        dataPacket.checksum = getChecksum(dataPacket.packet);
+        
+        ucpSendTo(socketID, &dataPacket, sizeof(DataPacket), getConnectionAddr(socketID));
+        curSequenceNum++;
+    }
+
+    int rcvPackets[numPackets];
+    memset(rcvPackets, 0, numPackets);
+    int packetsReceived = 0;
+    int curWindowLo = 0;
+    int curWindowHi = (numPackets < WINDOW_SIZE - 1)? numPackets : WINDOW_SIZE-1;
+    curSequenceNum = 0;
+
+    //now we receive them and move around our window accordingly
+    while (packetsReceived < numPackets) {
+
+        ucpSetSockRecvTimeout(socketID, 1000);
+
+        struct sockaddr_in addr;
+        int ackPacket[2];
+        ssize_t bytes = ucpRecvFrom(socketID, ackPacket, 4, &addr);
+        
+        // if ACK received:
+        if (bytes > 0) {
+            int seq = ackPacket[1];
+            // if in current window, mark packet as received
+            if (seq >= curWindowLo && seq <= curWindowHi) {
+                packetsReceived++;
+                rcvPackets[seq] = 1;
+            } else {
+                //TODO: what happens here? I beleive we ignore this
+            }
+
+            // if this packet's sequence number = send_base
+            if (seq == curWindowLo) {
+                // case 1: this is the ACK we expect -> move window
+                curWindowLo++;
+                curWindowHi++;
+            } else {
+                // case 2: this is greater than the ACK we expect -> retransmit ones in middle
+                for (i = curWindowLo; i<seq;i++) {
+
+                    DataPacket dataPacket;
+                    dataPacket.sequenceNum = curSequenceNum;
+                    dataPacket.packet = getNextPacket(sendBuffer, curSequenceNum);
+                    dataPacket.checksum = getChecksum(dataPacket.packet);
+                    
+                    ucpSendTo(socketID, &dataPacket, sizeof(DataPacket), getConnectionAddr(socketID));
+                }
+            }
+        }
+        else {
+            // case 3: we did not get an ACK back at all -> retransmit
+            // how much do we want to retransmit? one by one? all of them?
+        }
+            
+    }
+
+    return packetsReceived;
 } 
 
 //closes an RCS socket descriptor
